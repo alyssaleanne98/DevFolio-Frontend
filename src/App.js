4347cfefccf7ef006768d73698d7495ef9b7c772
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-11-17 13:21:21 -0800

    all components imported app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+//Import Components 
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom"
+
+//import pages
+import Home from "./pages/Home"
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+
 function App() {
+  // URL should have YOUR HEROKU URL for your backend, make sure you include the trailing slash
+  const URL = "http://localhost:4000/";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="projects" element={<Projects URL={URL}/>}/>
+        <Route path="/about" element={<About URL={URL} />}/>
+      </Routes>
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
